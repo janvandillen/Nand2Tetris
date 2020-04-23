ac56a94638d85598a12d753f7129875e4d252deb
j.vandillen <j.vandillen@criteo.com>
2020-04-23 11:50:18 +0200

    Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+    @i
+    M=1
+    @R2
+    M=0
+
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JGT
+    @R0
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
