533018e6d544c4a458f8698e07e5ad4dd7e37128
Jan van Dillen <jan@vandillen.eu>
2020-05-20 15:49:41 +0200

    first test

--- Tools/VMTranslator/vmTranslator.ps1
+++ Tools/VMTranslator/vmTranslator.ps1
@@ -2,23 +2,27 @@ Get-ChildItem -Path $PSScriptRoot -Filter *.vm -Recurse | ForEach-Object {
     $file = $_
     Write-Host -message ("processing: " + $file.Name)
     $content = Get-content -Path $file.FullName
-    $commanddict = @{};
-    $commandlist = @();
+    $commanddict = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath commandDict.json) | ConvertFrom-Json
+    $commandlist = @()
 
     foreach ($line in $content) {
         $line = $line -replace "//.*", ""
         $line = $line.trim()
+        if ($line -like "") {
+            continue
+        }
 
         $words = $line.split(" ")
 
         $command = $commanddict
 
         $i = 0
-        while ($true) {
+        $stop = $true
+        while ($stop) {
             switch ($command.next) {
-                "cmd" {$command = $command.$word[$i]}
-                "val" {$commandlist += $command.cmd -replace "#", $words[$i+1] ; break}
-                "stop" {$commandlist += $command.cmd ; break}
+                "cmd" {$command = $command.($words[$i])}
+                "val" {$commandlist += $command.cmd -replace "#", $words[$i] ; $stop = $false}
+                "stop" {$commandlist += $command.cmd ; $stop = $false}
                 Default {Write-Error -Message "no command" -ErrorAction Stop}
             }
             $i ++;
